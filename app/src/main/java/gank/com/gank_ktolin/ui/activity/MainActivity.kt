47e9e2880092beff8f45e72dffa25132ca9adf77
[xiongyanbing] <xyb520zly@gmail.com>
2019-07-17 17:59:16 +0800

    添加功能

--- app/src/main/java/gank/com/gank_ktolin/ui/activity/MainActivity.kt
+++ app/src/main/java/gank/com/gank_ktolin/ui/activity/MainActivity.kt
@@ -1,7 +1,8 @@
-package gank.com.gank_ktolin
+package gank.com.gank_ktolin.ui.activity
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import gank.com.gank_ktolin.R
 
 class MainActivity : AppCompatActivity() {
 
